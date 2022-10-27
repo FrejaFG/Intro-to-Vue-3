620cccf302920af609d55785869c0523de4d5c98
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-27 13:45:18 +0200

    image

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Nice time socks'
+            description: 'Nice time socks',
+            image: './assets/images/socks_green.jpeg'
         }
     }
 })
