b3224e63b110b1adaf7ce60cfe14ced3c1db8505
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-27 16:08:48 +0200

    Lesson 3 done

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'Nice time socks',
-            image: './assets/images/socks_green.jpeg'
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
         }
     }
 })
