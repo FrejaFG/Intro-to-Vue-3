945e8d7f8101e152dd3827f7c6ed695e4500db4f
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-27 18:21:02 +0200

    Lesson 5 done

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const app = Vue.createApp({
             url: 'https://www.vuemastery.com/',
             inventory: 10,
             onSale: false,
-            details: ["50% cotton", "30% wool", "20% polyester"]
+            details: ["50% cotton", "30% wool", "20% polyester"],
+            variants: [
+                { id: 2234, color: "green"},
+                { id: 2235, color: "blue"},
+            ],
+            sizes: ["XS", "S", "M", "L", "XL"]
+            
         }
     }
 })
