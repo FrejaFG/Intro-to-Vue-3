23278a99eccd5e4b6ceedfee42a6e0c0d61ce619
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-27 18:45:50 +0200

    Lesson6 done Shopping cart

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'Nice time socks',
-            image: './assets/images/socks_green.jpg',
+            image: './assets/images/socks_blue.jpg',
             url: 'https://www.vuemastery.com/',
             inventory: 10,
             onSale: false,
             details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-                { id: 2234, color: "green"},
-                { id: 2235, color: "blue"},
+                { id: 2234, color: "green", image: "./assets/images/socks_green.jpg"},
+                { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg"},
             ],
             sizes: ["XS", "S", "M", "L", "XL"]
             
+        }
+    },
+    methods: {
+        addToCart()
+        {
+            this.cart += 1
+        },
+        updateImage(variantImage)
+        {
+            this.image = variantImage
+        },
+        removeFromCart()
+        {
+            this.cart -= 1
         }
     }
 })
