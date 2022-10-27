ce7486b745c6a9be25b523a3862c6e38c26963c2
Freja Gudmandsen <ferling.gud@gmail.com>
2022-10-27 18:06:51 +0200

    Lesson 5 start

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const app = Vue.createApp({
             description: 'Nice time socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
-            inventory: 0,
-            onSale: false
+            inventory: 10,
+            onSale: false,
+            details: ["50% cotton", "30% wool", "20% polyester"]
         }
     }
 })
